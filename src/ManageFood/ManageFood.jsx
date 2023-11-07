3c866a93e2e3b40d74a0d440e2bd1386ca96dfa6
Pranto-Sen <prantosen56@gmail.com>
2023-11-08 05:12:56 +0600

    food delete option added

--- src/ManageFood/ManageFood.jsx
+++ src/ManageFood/ManageFood.jsx
@@ -50,9 +50,11 @@
 import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../Providers/AuthProvider";
 import DataTable from "react-data-table-component";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const ManageFood = () => {
+  const navigate = useNavigate();
   const { user } = useContext(AuthContext);
   const email = user.email;
   // let _id = user._id;
@@ -64,6 +66,40 @@ const ManageFood = () => {
       .then((res) => res.json())
       .then((data) => setItems(data));
   }, [email]); // Added dependency to the useEffect
+
+  const handleDelete = (id) => {
+    
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.value) {
+        fetch(`http://localhost:5000/food/${id}`, {
+          method: 'DELETE',
+        })
+        .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+           Swal.fire({
+             text: "Delete Successfully",
+             icon: "success",
+             confirmButtonText: "Done",
+           });
+              const remaining = items.filter(item => item._id !== id);
+              setItems(remaining);
+           navigate("/manageFood");
+        }
+      });
+        // form.submit();
+        console.log(id);
+      }
+    });
+  };
 
   const columns = [
     {
@@ -90,8 +126,13 @@ const ManageFood = () => {
     {
       name: "",
       selector: (row) => (
-        <button className="bg-red-300 px-6 py-2 rounded-md text-base font-semibold">
-          Delect
+        <button
+          onClick={() => {
+            handleDelete(row._id);
+          }}
+          className="bg-red-300 px-6 py-2 rounded-md text-base font-semibold"
+        >
+          Delete
         </button>
       ),
     },
@@ -107,8 +148,7 @@ const ManageFood = () => {
 
   return (
     <div>
-      
-      <DataTable  columns={columns} data={items} />
+      <DataTable columns={columns} data={items} />
     </div>
   );
 };
